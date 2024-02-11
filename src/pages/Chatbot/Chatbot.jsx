bce738d87c240bf225dbc205807bc39da7a2da67
LintaRahman <zcablra@ucl.ac.uk>
2024-02-11 10:39:44 +0000

    Updated Voice Recorder

--- src/pages/Chatbot/Chatbot.jsx
+++ src/pages/Chatbot/Chatbot.jsx
@@ -20,12 +20,18 @@ import {
   faDownload,
   faArrowsRotate,
   faComments,
+  faMicrophone,
+  faMicrophoneSlash,
 } from "@fortawesome/free-solid-svg-icons";
 import convertSpeechToText from "../../services/SpeechToText.js";
-import RecordMessage from "../../components/RecordMessage/RecordMessage.jsx";
+// import RecordMessage from "../../components/RecordMessage/RecordMessage.jsx";
 import { ReactMediaRecorder } from "react-media-recorder";
 import RecordIcon from "../../components/RecordMessage/RecordIcon.jsx";
 import { Link } from "react-router-dom";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
 
 const parameters = JSON.parse(localStorage.getItem("formData"));
 
@@ -45,6 +51,19 @@ const profImgStyle = {
   color: "rgba(28, 30, 58, 1)", //"rgba(28, 30, 15, 1)",
 };
 
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+
 const Chatbot = () => {
   const [pageLoading, isPageLoading] = useState(true);
   const [EndOfInterview, isEndOfInterview] = useState(false);
@@ -54,6 +73,10 @@ const Chatbot = () => {
   const [TranscriptLoading, setTranscriptLoading] = useState(false);
   const [prompt, setPrompt] = useState("");
   const [feedback, setFeedback] = useState("");
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
 
   const [end, setEnd] = useState(false);
   const [recordingStatus, setRecordingStatus] = useState(false);
@@ -115,16 +138,19 @@ const Chatbot = () => {
   };
 
   const handleStop = async (audioUrl) => {
+    console.log(audioUrl);
     setTranscriptLoading(true);
+
     try {
       const transcript = await convertSpeechToText(audioUrl);
-      // console.log("Transcription:", transcript.data.text);
+      console.log("Transcription:", transcript);
       // != "" ? transcript : prompt;
       // const transcriptText = transcript.data.text ? transcript.data.text : '';
-      const text = transcript.data.text;
-      setMessages([...messages, { text, isBot: false, audio: audioUrl }]);
-      console.log({ text, isBot: false, audio: audioUrl });
-      if (text != "") {
+
+      if (transcript && transcript.data && transcript.data.text) {
+        const text = transcript.data.text;
+        setMessages([...messages, { text, isBot: false, audio: audioUrl }]);
+        // console.log({ text, isBot: false, audio: audioUrl });
         setTranscriptLoading(false);
         const res = await sendMsgToOpenAI(text);
         setMessages([
@@ -133,16 +159,16 @@ const Chatbot = () => {
           { text: res, isBot: true, audio: "" },
         ]);
 
-        console.log(messages);
-        checkEndOfInterview();        
+        // console.log(messages);
+        checkEndOfInterview();
+      } else {
+        // Handle case where transcript is not available
+        console.error("Transcript is not available.");
       }
     } catch (error) {
       console.error("Error converting speech to text:", error);
+    } finally {
       setTranscriptLoading(false);
-      //   setMessages([
-      //     ...messages,
-      //     { text: "I didn't catch that.", isBot: true, audio: "" },
-      //   ]);
     }
   };
 
@@ -158,89 +184,104 @@ const Chatbot = () => {
     }
   };
 
-  // const mimeType = "audio/webm";
-  // const [permission, setPermission] = useState(true); //REMEMBER TO SET TO FALSE
-  // const mediaRecorder = useRef(null);
+  const [permission, setPermission] = useState(false); //REMEMBER TO SET TO FALSE
+  const mediaRecorder = useRef(null);
 
-  // const [stream, setStream] = useState(null);
-  // const [audio, setAudio] = useState([]);
-  // const [audioChunks, setAudioChunks] = useState([]);
+  const [stream, setStream] = useState(null);
+  const [audio, setAudio] = useState([]);
+  const [audioChunks, setAudioChunks] = useState([]);
 
-  // const getMicrophonePermission = async () => {
-  //   if ("MediaRecorder" in window) {
-  //     try {
-  //       const mediaStream = await navigator.mediaDevices.getUserMedia({
-  //         audio: true,
-  //       });
-  //       setPermission(true);
-  //       setStream(mediaStream);
-  //     } catch (err) {
-  //       alert(err.message);
-  //     }
-  //   } else {
-  //     alert("The MediaRecorder API is not supported in your browser.");
-  //   }
-  // };
+  const getMicrophonePermission = async () => {
+    if ("MediaRecorder" in window) {
+      try {
+        const mediaStream = await navigator.mediaDevices.getUserMedia({
+          audio: true,
+        });
+        setPermission(true);
+        setStream(mediaStream);
+      } catch (err) {
+        alert(err.message);
+      }
+    } else {
+      alert("The MediaRecorder API is not supported in your browser.");
+    }
+  };
 
-  // const startRecording = async () => {
-  //   await getMicrophonePermission();
-  //   console.log("Started Recording");
-  //   setRecordingStatus(true);
-  //   const media = new MediaRecorder(stream, { type: mimeType });
+  const startRecording = async () => {
+    try {
+      if (!stream || !(stream instanceof MediaStream)) {
+        // If stream is not set, request microphone permission
+        await getMicrophonePermission();
+      }
+      if (stream instanceof MediaStream) {
+        console.log("Started Recording");
+        setRecordingStatus(true);
+        const media = new MediaRecorder(stream, { type: "audio/webm" });
+  
+        mediaRecorder.current = media;
+  
+        mediaRecorder.current.start();
+  
+        let localAudioChunks = [];
+  
+        mediaRecorder.current.ondataavailable = (event) => {
+          if (typeof event.data === "undefined") return;
+          if (event.data.size === 0) return;
+          localAudioChunks.push(event.data);
+        };
+  
+        setAudioChunks(localAudioChunks);
+      } else {
+        console.error("MediaStream is not available.");
+      }
+    } catch (error) {
+      console.error("Error starting recording:", error);
+    }
+  };
 
-  //   mediaRecorder.current = media;
+  const stopRecording = () => {
+    console.log("Stopped Recording");
 
-  //   mediaRecorder.current.start();
+    setRecordingStatus(false);
+    if (mediaRecorder.current) {
+      mediaRecorder.current.stop();
 
-  //   let localAudioChunks = [];
+      mediaRecorder.current.onstop = async () => {
+        const audioBlob = new Blob(audioChunks, { type: "audio/webm" });
+        const audioUrl = URL.createObjectURL(audioBlob);
+        console.log("Inside");
+        setAudio([...audio, audioUrl]);
 
-  //   mediaRecorder.current.ondataavailable = (event) => {
-  //     if (typeof event.data === "undefined") return;
-  //     if (event.data.size === 0) return;
-  //     localAudioChunks.push(event.data);
-  //   };
+        setAudioChunks([]);
+        setTranscriptLoading(true);
+        try {
+          const transcript = await convertSpeechToText(audioUrl);
+          setTranscript(transcript.data.text);
+          console.log("Transcription:", transcript.data.text);
 
-  //   setAudioChunks(localAudioChunks);
-  // };
+          const text = transcript.data.text; // != "" ? transcript : prompt;
 
-  // const stopRecording = () => {
-  //   console.log("Stopped Recording");
+          setTranscript("");
+          setMessages([...messages, { text, isBot: false, audio: audioUrl }]);
+          setTranscriptLoading(false);
 
-  //   setRecordingStatus(false);
-  //   if (mediaRecorder.current) {
-  //     mediaRecorder.current.stop();
+          const res = await sendMsgToOpenAI(text);
+          setMessages([
+            ...messages,
+            { text: text, isBot: false, audio: audioUrl },
+            { text: res, isBot: true, audio: "" },
+          ]);
 
-  //     mediaRecorder.current.onstop = async () => {
-  //       const audioBlob = new Blob(audioChunks, { type: mimeType });
-  //       const audioUrl = URL.createObjectURL(audioBlob);
-  //       console.log("Inside");
-  //       setAudio([...audio, audioUrl]);
-
-  //       setAudioChunks([]);
-  //       try {
-  //         const transcript = await convertSpeechToText(audioUrl);
-  //         setTranscript(transcript.data.text);
-  //         console.log('Transcription:', transcript.data.text);
-  //         setTranscriptLoading(true);
-
-  //         const text = transcript.data.text;// != "" ? transcript : prompt;
-
-  //         setTranscript("");
-  //         setMessages([...messages, { text, isBot: false, audio: audioUrl }]);
-  //         const res = await sendMsgToOpenAI(transcript.data.text);
-  //         setTranscriptLoading(false);
-  //         setMessages([
-  //           ...messages,
-  //           { text: transcript.data.text, isBot: false, audio: audioUrl },
-  //           { text: res, isBot: true, audio: "" },
-  //         ]);
-  //         // Perform any further actions with the transcriptMsg
-  //       } catch (error) {
-  //         console.error("Error converting speech to text:", error);
-  //       }
-  //     };
-  //   }
-  // };
+          console.log(messages);
+          checkEndOfInterview();
+          // Perform any further actions with the transcriptMsg
+        } catch (error) {
+          setTranscriptLoading(false);
+          console.error("Error converting speech to text:", error);
+        }
+      };
+    }
+  };
 
   // State for chat messages and user input
 
@@ -281,7 +322,7 @@ const Chatbot = () => {
                               }}
                             >
                               <div style={{ padding: "5px" }}>
-                                <AudioComponent msg={message.text} /> 
+                                {/* <AudioComponent msg={message.text} />  */}
                               </div>
                               <div>
                                 <TypeAnimation
@@ -345,15 +386,14 @@ const Chatbot = () => {
                         <ChatLoader />
                       ) : (
                         <>
-                        
-                        <TypeAnimation
-                          sequence={[feedback]}
-                          wrapper="p"
-                          speed={90}
-                          cursor="none"
-                          repeat={1}
-                        />
-                        <AudioComponent msg={feedback}/>
+                          <TypeAnimation
+                            sequence={[feedback]}
+                            wrapper="p"
+                            speed={90}
+                            cursor="none"
+                            repeat={1}
+                          />
+                          {/* <AudioComponent msg={feedback} /> */}
                         </>
                       )}
                     </p>
@@ -384,22 +424,29 @@ const Chatbot = () => {
                       </button>
                     </div>
                     <div className="chatsButtonsContainer">
-                      <Link to='https://qualtricsxm7gwbjys5f.qualtrics.com/jfe/form/SV_eFBgfGbd5xhvemi' target='_blank'>
-                      <button
-                        className="midBtn"
+                      <Link
+                        to="https://qualtricsxm7gwbjys5f.qualtrics.com/jfe/form/SV_eFBgfGbd5xhvemi"
+                        target="_blank"
                       >
-                        <FontAwesomeIcon icon={faComments} style={iconStyle} />
-                        Complete Survey
-                      </button></Link>
+                        <button className="midBtn">
+                          <FontAwesomeIcon
+                            icon={faComments}
+                            style={iconStyle}
+                          />
+                          Complete Survey
+                        </button>
+                      </Link>
                     </div>
                   </>
                 )}
                 <div ref={msgEnd} />
               </div>
-              {EndOfInterview ? '' :
-              <div className="chatFooter">
-              <div className="inp">
-                <ReactMediaRecorder
+              {EndOfInterview ? (
+                ""
+              ) : (
+                <div className="chatFooter">
+                  <div className="inp">
+                    {/* <ReactMediaRecorder
                   audio
                   onStop={handleStop}
                   render={({ status, startRecording, stopRecording }) => (
@@ -417,44 +464,97 @@ const Chatbot = () => {
                       {status === "recording" ? (
                         <div className="recording-dot" />
                       ) : null}
-                      {/* <p className="mt-2 text-white font-light">{status}</p> */}
+                     <p className="mt-2 text-white font-light">{status}</p> 
                     </div>
                   )}
-                />
-                {/* <RecordMessage handlestop={handleStop} /> */}
-                {/* {permission && !recordingStatus? (
-              <button onClick={startRecording} className="send">
-                <FontAwesomeIcon icon={faMicrophoneSlash} style={iconStyle} />
-              </button>
-            ) : null} */}
+                /> */}
+                    {/* <RecordMessage handlestop={handleStop} /> */}
+                    {!permission ? (
+                      <Modal
+                        open={open}
+                        onClose={handleClose}
+                        aria-labelledby="modal-modal-title"
+                        aria-describedby="modal-modal-description"
+                      >
+                        <Box sx={style}>
+                          <Typography
+                            id="modal-modal-title"
+                            variant="h6"
+                            component="h2"
+                          >
+                            Allow access to microphone
+                          </Typography>
+                          <Typography
+                            id="modal-modal-description"
+                            sx={{ mt: 2 }}
+                          >
+                            Duis mollis, est non commodo luctus, nisi erat
+                            porttitor ligula.
+                          </Typography>
+                        </Box>
+                      </Modal>
+                    ) : null}
 
-                {/* {recordingStatus? (
-              <div className="recording-animation-container">
-              <button onClick={stopRecording} className="send">
-                <FontAwesomeIcon icon={faMicrophone} style={iconStyle} />
-              </button>
-              <div className="recording-dot" />
-              </div>
+                    {!recordingStatus ? (
+                      <><Modal
+                      open={open}
+                      onClose={handleClose}
+                      aria-labelledby="modal-modal-title"
+                      aria-describedby="modal-modal-description"
+                    >
+                      <Box sx={style}>
+                        <Typography
+                          id="modal-modal-title"
+                          variant="h6"
+                          component="h2"
+                        >
+                          Allow access to microphone
+                        </Typography>
+                        <Typography
+                          id="modal-modal-description"
+                          sx={{ mt: 2 }}
+                        >
+                          Duis mollis, est non commodo luctus, nisi erat
+                          porttitor ligula.
+                        </Typography>
+                      </Box>
+                    </Modal>
+                      <button onClick={startRecording} className="send">
+                        <FontAwesomeIcon
+                          icon={faMicrophoneSlash}
+                          style={iconStyle}
+                        />
+                      </button></>
+                    ) : null}
+                    {recordingStatus ? (
+                      <div className="recording-animation-container">
+                        <button onClick={stopRecording} className="send">
+                          <FontAwesomeIcon
+                            icon={faMicrophone}
+                            style={iconStyle}
+                          />
+                        </button>
+                        <div className="recording-dot" />
+                      </div>
+                    ) : null}
 
-            ) : null} */}
-
-                <input
-                  id="UserInput"
-                  type="text"
-                  placeholder="Send a message"
-                  value={prompt}
-                  onKeyDown={handleEnter}
-                  onChange={(e) => {
-                    setPrompt(e.target.value);
-                  }}
-                />
-                <button className="send" onClick={handleSend}>
-                  <FontAwesomeIcon icon={faPaperPlane} style={iconStyle} />
-                </button>
-              </div>
-              <p>Powered by ChatGPT 3.5. </p>
-            </div>}
-              
+                    <input
+                      id="UserInput"
+                      type="text"
+                      placeholder="Send a message"
+                      value={prompt}
+                      onKeyDown={handleEnter}
+                      onChange={(e) => {
+                        setPrompt(e.target.value);
+                      }}
+                    />
+                    <button className="send" onClick={handleSend}>
+                      <FontAwesomeIcon icon={faPaperPlane} style={iconStyle} />
+                    </button>
+                  </div>
+                  <p>Powered by ChatGPT 3.5. </p>
+                </div>
+              )}
             </div>
           </div>
         </>
--- src/services/SpeechToText.js
+++ src/services/SpeechToText.js
@@ -1,9 +1,12 @@
 
 import FormData from "form-data";
 import axios from 'axios';
+import OpenAI from "openai";
 
 const OpenAIAPIKey = import.meta.env.VITE_REACT_OPENAI_API_KEY;
-
+const openai = new OpenAI({
+  apiKey: import.meta.env.VITE_REACT_OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true});
 // const parameters = JSON.parse(localStorage.getItem("formData"));
 // const findLangKey = ({language}) => {
 //   const lowercasedLanguageName = language;
@@ -20,6 +23,19 @@ const convertSpeechToText = async (blobURL) => {
         // Fetch the Blob from the Blob URL
         const responseFromFetch = await fetch(blobURL);
         const blob = await responseFromFetch.blob();
+
+        // const transcription = await openai.audio.transcriptions.create(
+        //   {
+        //     model: "whisper-1", 
+        //     file: blob,
+        //     type: 'audio.webm',
+        //     response_format:"text", 
+        //     language: 'en',
+        //   }
+        // )
+
+        // console.log("NewTRanscript", transcription);
+
         let data = new FormData();
         data.append("file", blob, "audio.webm");
         data.append("model", "whisper-1");
